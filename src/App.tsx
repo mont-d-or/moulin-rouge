7cc2d0ad1131069d0cd592bb6322ee79dfcccc1c
EQuincerot <18597999+EQuincerot@users.noreply.github.com>
2022-10-23 09:36:02 +0200

    Use momentjs to manage dates
    pnpm install moment --save

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import "react-datepicker/dist/react-datepicker.css"
 import DatePicker from "react-datepicker"
-import { setDate } from 'date-fns'
+import 'moment'
+import moment from 'moment'
+
+const loadFromLocalStorage: () => Array<moment.Moment> = () => {
+  const loadedDates: Array<moment.Moment> = []
+  const storedDates = window.localStorage.getItem('previousDates')
+  if (storedDates != null) {
+    loadedDates.push(...JSON.parse(storedDates).map((dateString: moment.MomentInput) => moment(dateString, "YYYY-MM-DD")))
+  }
+  return loadedDates
+}
+
+const saveIntoLocalStorage = (dates: Array<moment.Moment>) => {
+  const localStorageString = JSON.stringify(dates.map((date) => date.format("YYYY-MM-DD")))
+  window.localStorage.setItem('previousDates', localStorageString)
+}
 
 const App = () => {
-  const [previousDates, setPreviousDates] = useState<Array<Date>>([])
+  const [previousDates, setPreviousDates] = useState<Array<moment.Moment>>([])
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+
+  useEffect(() => {
+    setPreviousDates(loadFromLocalStorage())
+  }, [])
 
   const addDateToPreviousDates = () => {
     if (selectedDate == null) {
       return
     }
-    previousDates.push(selectedDate)
+    previousDates.push(moment(selectedDate))
     setSelectedDate(null)
+    saveIntoLocalStorage(previousDates)
   }
 
   return (
@@ -21,7 +41,7 @@ const App = () => {
       <h1>Moulin Rouge</h1>
       <div>
         {
-          previousDates.map(d => <div>{d.toDateString()}</div>)
+          previousDates.map(d => <div key={d.toString()}>{d.format('DD-MM-YYYY')}</div>)
         }
       </div>
       <div className="card">
