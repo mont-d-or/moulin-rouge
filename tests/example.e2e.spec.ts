34cdcc0271b957fc42b102502c4884390a521736
EQuincerot <18597999+EQuincerot@users.noreply.github.com>
2023-10-07 16:24:39 +0200

    Fix playwright test: force current date to a given date

--- tests/example.e2e.spec.ts
+++ tests/example.e2e.spec.ts
@@ -1,6 +1,32 @@
 import { type Page, expect, test } from '@playwright/test'
 
+// Pick the new/fake "now" for you test pages.
+const fakeNow = new Date('October 10 2022 13:01:02').valueOf()
+
+// https://github.com/microsoft/playwright/issues/6347
+// Update the Date accordingly in your test pages
+const fakeDateTime = async (page: Page) => {
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+  await page.addInitScript(`{
+    // Extend Date constructor to default to fakeNow
+    Date = class extends Date {
+      constructor(...args) {
+        if (args.length === 0) {
+          super(${fakeNow});
+        } else {
+          super(...args);
+        }
+      }
+    }
+    // Override Date.now() to start from fakeNow
+    const __DateNowOffset = ${fakeNow} - Date.now();
+    const __DateNow = Date.now;
+    Date.now = () => __DateNow() + __DateNowOffset;
+  }`)
+}
+
 const openPage = async (page: Page) => {
+  await fakeDateTime(page)
   await page.goto('http://localhost:5173/')
 }
 
